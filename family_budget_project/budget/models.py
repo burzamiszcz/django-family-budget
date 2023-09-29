659c40b7adc4a0cb057e4d7bb7fe999143dea4c1
burzamiszcz <burzynskihubert@gmail.com>
2023-09-29 16:32:56 +0200

    create models

--- family_budget_project/budget/models.py
+++ family_budget_project/budget/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    shared_with = models.ManyToManyField(User, related_name='shared_budgets', blank=True)
+
+class Income(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    source = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+class Expense(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
