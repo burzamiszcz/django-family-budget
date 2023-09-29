3d2d7164eb4052694f8aa3ed9ce3ce58daa6c995
burzamiszcz <burzynskihubert@gmail.com>
2023-09-29 16:33:31 +0200

    create serializers for models

---
+++ family_budget_project/budget/serializers.py
@@ -0,0 +1,31 @@
+
+from rest_framework import serializers
+from .models import Budget, Income, Expense, Category
+from django.contrib.auth.models import User
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class IncomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Income
+        fields = '__all__'
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = '__all__'
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = '__all__'
+
+class BudgetDetailsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Budget
+        fields = '__all__'
