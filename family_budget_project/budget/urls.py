5d7ede88321c311b3df3fc17c5ef5719b84156d8
burzamiszcz <burzynskihubert@gmail.com>
2023-10-01 18:09:07 +0200

    add more urls

--- family_budget_project/budget/urls.py
+++ family_budget_project/budget/urls.py
@@ -4,4 +4,9 @@ from .views import *
 urlpatterns = [
     path('budgets/', BudgetListCreateView.as_view(), name='budget-list-create'),
     path('budgets/<int:pk>', BudgetDetailsView.as_view(), name='budget-details'),
+    path('budgets/<int:pk>/incomes/create/', IncomeCreateView.as_view(), name='income-create'),
+    path('budgets/<int:pk>/expenses/create/', ExpenseCreateView.as_view(), name='expense-create'),
+    path('budgets/<int:pk>/share/update', ShareBudgetUpdateView.as_view(), name='share-budget-update'),
+    path('budgets/<int:pk>/share/delete', ShareBudgetDeleteView.as_view(), name='share-budget-delete'),
+
 ]
